a45809d0530bc394372ee9c711ebaceaa788e29e
Niclas Axelsson <niclas@burbas.se>
2010-10-22 18:52:15 +0200

    Changed the types to use big endian.

--- include/types.hrl
+++ include/types.hrl
@@ -2,10 +2,10 @@
 %%% Types used in the server
 %%% @end
 
--define(mc_byte(X), X:8/native-signed-integer).
--define(mc_short(X), X:16/native-signed-integer).
--define(mc_int(X), X:32/native-signed-integer).
--define(mc_long(X), X:64/native-signed-integer).
--define(mc_float(X), X:32/native-float).
--define(mc_double(X), X:64/native-float).
--define(mc_bool(X), X:8/native-unsigned-integer).
+-define(mc_byte(X), X:8/big-signed-integer).
+-define(mc_short(X), X:16/big-signed-integer).
+-define(mc_int(X), X:32/big-signed-integer).
+-define(mc_long(X), X:64/big-signed-integer).
+-define(mc_float(X), X:32/big-float).
+-define(mc_double(X), X:64/big-float).
+-define(mc_bool(X), X:8/big-unsigned-integer).
